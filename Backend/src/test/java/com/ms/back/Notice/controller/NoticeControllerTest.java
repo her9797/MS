68afc046359241abade2fb915dc3f413ccf2bb05
her97 <simkoon0120@gmail.com>
2024-07-13 00:22:08 +0900

    TEST 파일 생성

---
+++ Backend/src/test/java/com/ms/back/Notice/controller/NoticeControllerTest.java
@@ -0,0 +1,13 @@
+package com.ms.back.Notice.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeControllerTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- Backend/src/test/java/com/ms/back/Notice/service/NoticeServiceTest.java
+++ Backend/src/test/java/com/ms/back/Notice/service/NoticeServiceTest.java
@@ -1,10 +1,10 @@
-package com.ms.back;
+package com.ms.back.Notice.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class NoticeServiceTest {
 
     @Test
     void contextLoads() {
