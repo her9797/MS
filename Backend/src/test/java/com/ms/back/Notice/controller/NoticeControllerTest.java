efa35c3a6aa5e94e746ac09b4fc9e799455ba1f8
her97 <simkoon0120@gmail.com>
2024-07-30 00:01:19 +0900

    공지사항 수정 컨트롤러 TDD

--- Backend/src/test/java/com/ms/back/Notice/controller/NoticeControllerTest.java
+++ Backend/src/test/java/com/ms/back/Notice/controller/NoticeControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.web.servlet.MvcResult;
 
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -75,6 +76,42 @@ class NoticeControllerTest {
         String content = result.getResponse().getContentAsString();
         System.out.println("response content : " + content);
 
+    }
+
+    @Test
+    @DisplayName("공지사항 수정 테스트")
+    void modifyNotice() throws Exception {
+
+        // given
+        int noticeNo = 2;
+
+        NoticeDTO noticeDTO = new NoticeDTO(
+                "컨트롤러 본문 수정 테스트",
+                "컨트롤러 제목 수정",
+                LocalDateTime.now()
+        );
+
+        String modifiedDateStr = noticeDTO.getModifiedDate().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+
+        // JSON request body 생성
+        String requestBody = String.format(
+                "{\"content\": \"%s\", \"title\": \"%s\", \"modifiedDate\": \"%s\"}",
+                noticeDTO.getContent(), noticeDTO.getTitle(), modifiedDateStr
+        );
+
+        // when
+        MvcResult result = mockMvc.perform(patch("/notices/{noticeNo}", noticeNo)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                // then
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.httpStatusCode").value(200))
+                .andExpect(jsonPath("$.message").value("수정 성공"))
+                .andExpect(jsonPath("$.results").exists())
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+        System.out.println("Content: " + content);
     }
 
 }
