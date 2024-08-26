164c2c9c9de14e479c038f1dc40515c60be4856d
her97 <simkoon0120@gmail.com>
2024-08-26 23:44:36 +0900

    #4-icon/name

--- frontend/src/components/common/Preview.js
+++ frontend/src/components/common/Preview.js
@@ -4,9 +4,11 @@ function Preview() {
         <div class="row gx-4 gx-lg-5 align-items-center my-5">
             <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="./../img/main.jpg" alt="..." /></div>
             <div class="col-lg-5">
-                <h1 class="font-weight-light">안녕하세요 <br/>MS 커뮤니티 입니다</h1>
+                <h1 class="font-weight-light">WelCome!<br/></h1>
+                <p></p>
+                
                 <p>그냥 재미삼아 자유 커뮤니티 하나 만들어보고 있습니다 <br/> 관련 소스코드는 아래에 있습니다 참고 부탁드립니다</p>
-                <a class="btn btn-primary" href="#!">Visit GitHub</a>
+                <a class="btn btn-primary" href="https://github.com/her9797/MS">Visit GitHub</a>
             </div>
         </div>
     </div>
