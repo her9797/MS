bc3f53109bf7733f004bac249b0e8d1f56c4b10a
her97 <simkoon0120@gmail.com>
2024-09-04 00:40:19 +0900

    chatItem/List/apiCalls

---
+++ frontend/src/modules/RoomModules.js
@@ -0,0 +1,17 @@
+import { createActions, handleActions } from 'redux-actions';
+
+const initialState = {
+    roomAndUser: [],
+};
+
+export const GET_ROOMS = 'rooms/GET_ROOMS';
+
+const actions = createActions({
+    [GET_ROOMS]: () => {}
+});
+
+const roomReducer = handleActions({
+    [GET_ROOMS]: (state, {payload}) => ({roomAndUser: payload})
+}, initialState);
+
+export default roomReducer;
