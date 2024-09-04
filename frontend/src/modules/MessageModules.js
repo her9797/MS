e87dfee93da94a6e107dabd09234305917166fd1
her97 <simkoon0120@gmail.com>
2024-09-04 23:45:22 +0900

    js 파일 생성

---
+++ frontend/src/modules/MessageModules.js
@@ -0,0 +1,17 @@
+import { createActions, handleActions } from 'redux-actions';
+
+const initialState = {
+
+};
+
+export const GET_MESSAGES = 'messages/GET_MESSAGES';
+
+const actions = createActions({
+    [GET_MESSAGES]: () => {}
+});
+
+const messageReducer = handleActions({
+    [GET_MESSAGES]: (state, {payload}) => ({msg: payload})
+}, initialState);
+
+export default messageReducer;
