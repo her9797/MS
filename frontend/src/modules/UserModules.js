7293da3360736656cb622d24edc4628e3d87a21a
her9797 <simkoon0120@gmail.com>
2024-09-15 21:07:11 +0900

    LOGIN-POST

--- frontend/src/modules/UserModules.js
+++ frontend/src/modules/UserModules.js
@@ -5,13 +5,16 @@ const initialState = {
 };
 
 export const POST_USER = 'users/POST_USER';
+export const POST_LOGIN_USER = 'users/POST_LOGIN_USER';
 
 const actions = createActions({
     [POST_USER]: () => {},
+    [POST_LOGIN_USER]: () => {}
 });
 
 const userReducer = handleActions({
     [POST_USER]: (state, {payload}) => ({user: payload}),
+    [POST_LOGIN_USER]: (state, {payload}) => ({payload}),
 }, initialState);
 
 export default userReducer;
